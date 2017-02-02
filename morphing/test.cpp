412e6811b2b287b2f0874780e552f24881d93c5a
Gurleen <gdhody@iu.edu>
2017-02-01 19:33:15 -0500

    Morphing Code

---
+++ morphing/test.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+int main(){
+
+srand(50);
+printf("%d\n",rand()%2);
+printf("%d\n",rand()%2);
+printf("%d\n",rand()%2);
+printf("%d\n",rand()%2);
+printf("%d\n",rand()%2);
+
+
+return 0;
+}
