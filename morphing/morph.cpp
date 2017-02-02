ad09edd521f19975fb37b19ff9673566c8d10639
Rohil <bansalro@indiana.edu>
2017-02-01 20:26:55 -0500

    Morphing_3

--- morphing/morph.cpp
+++ morphing/morph.cpp
@@ -10,16 +10,32 @@ using namespace std;
 
 int main(int argc, char** argv){
 
-	SDoublePlane gaussian = SDoublePlane(3,3);
-	gaussian[0][0] = 0.075;
-	gaussian[0][1] = 0.125;
-	gaussian[0][2] = 0.075;
-	gaussian[1][0] = 0.125;
-	gaussian[1][1] = 0.200;
-	gaussian[1][2] = 0.125;
-	gaussian[2][0] = 0.075;
-	gaussian[2][1] = 0.125;
-	gaussian[2][2] = 0.075;
+	SDoublePlane gaussian = SDoublePlane(5,5);
+	gaussian[0][0] = 0.039206;
+	gaussian[0][1] = 0.039798;
+	gaussian[0][2] = 0.039997;
+	gaussian[0][3] = 0.039798;
+	gaussian[0][4] = 0.039206;
+	gaussian[1][0] = 0.039798;
+	gaussian[1][1] = 0.040399;
+	gaussian[1][2] = 0.040601;
+	gaussian[1][3] = 0.040399;
+	gaussian[1][4] = 0.039798;
+	gaussian[2][0] = 0.039997;
+	gaussian[2][1] = 0.040601;
+	gaussian[2][2] = 0.040804;
+	gaussian[2][3] = 0.040601;
+	gaussian[2][4] = 0.039997;
+	gaussian[3][0] = 0.039798;
+	gaussian[3][1] = 0.040399;
+	gaussian[3][2] = 0.040601;
+	gaussian[3][3] = 0.040399;
+	gaussian[3][4] = 0.039798;
+	gaussian[4][0] = 0.039206;
+	gaussian[4][1] = 0.039798;
+	gaussian[4][2] = 0.039997;
+	gaussian[4][3] = 0.039798;
+	gaussian[4][4] = 0.039206;
 	
 	string inputFile_1 = argv[1];
 	string inputFile_2 = argv[2];
@@ -40,6 +56,7 @@ int main(int argc, char** argv){
 			}	
 		}
 	}
+	SImageIO::write_png_file("lowPass_1.png", lowPass_1, lowPass_1, lowPass_1);
 
 	for(int i=1 ; i<input_2.rows()-1; i++){
 		for(int j=1; j<input_2.cols()-1; j++){
@@ -50,12 +67,14 @@ int main(int argc, char** argv){
 			}	
 		}
 	}
+	SImageIO::write_png_file("lowPass_2.png", lowPass_2, lowPass_2, lowPass_2);
 
 	for(int i=1; i<input_2.rows()-1; i++){
 		for(int j=1; j<input_2.cols()-1; j++){
 			highPass[i][j] = input_2[i][j] - lowPass_2[i][j];
 		}
 	}
+	SImageIO::write_png_file("highPass.png", highPass, highPass, highPass);
 
 	for(int i=1; i<input_2.rows()-1; i++){
 		for(int j=1; j<input_2.cols()-1; j++){
